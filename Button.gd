c27e3760a83f57f16fdf3653b42bbac2ff79f3bf
Nicholas Orlowski <nick.orlowski@gmail.com>
2021-03-20 14:11:43 -0400

    Update path

--- Button.gd
+++ Button.gd
@@ -3,4 +3,4 @@ extends Control
 onready var button = $Button
 
 func _ready():
-	ui_signals.register_tooltip(button,"Welcome to Godot! \n\n You can do basic text here\n\n")
+	ui_signals.register_tooltip(button,"\nWelcome to Godot! \n You can do basic text here\n")
